c3f3de90f7e0415922fb44d88481c94982c4050e
Sergey Parshukov <codedby@bugman.me>
2017-07-01 15:37:56 +0300

    Separated from main repo

---
+++ README.md
@@ -0,0 +1,3 @@
+### Translator from Go to Fun
+
+This project provides unfinished translator from Go to [Fun](https://github.com/jBugman/fun-lang). Currently development in on pause. 
---
+++ fromgo/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"go/parser"
+	"go/token"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/jBugman/fun-translate/translate"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		exit("Usage: fun-translate [filename.go]")
+	}
+	infile := os.Args[1]
+	outfile := strings.TrimSuffix(infile, filepath.Ext(infile)) + ".fun"
+	var err error
+
+	// Read from source
+	source, err := ioutil.ReadFile(infile)
+	if err != nil {
+		exit(err)
+	}
+
+	// Parse Go AST
+	fset := token.NewFileSet()
+	ast, err := parser.ParseFile(fset, infile, source, 0)
+	if err != nil {
+		exit(err)
+	}
+
+	// Translate to Fun
+	pkg, err := translate.NewTranslator(fset).Package(ast)
+	if err != nil {
+		exit(err)
+	}
+	// result =
+
+	// Write result to file
+	err = ioutil.WriteFile(outfile, []byte(fmt.Sprint(pkg)), 0644)
+	if err != nil {
+		exit(err)
+	}
+}
+
+func exit(err interface{}) {
+	fmt.Fprintln(os.Stderr, err)
+	os.Exit(1)
+}
